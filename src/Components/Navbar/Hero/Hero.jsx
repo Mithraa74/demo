a44566f186b36a3f91cd4afd21acff2117df73bd
Mithraa74 <miithraask74@gmail.com>
2024-10-10 13:56:16 +0530

    first commit

---
+++ src/Components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        
+        <img src="" alt="" />
+        <h1><span>I'm Mithraa</span></h1>
+        <p>I am a frontend developer</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with me</div>
+            <div className="hero-resume">My resume</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
